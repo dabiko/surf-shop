a1cf64bb8045429696e96cdd6f99ec9df1f98127
dabiko <dabiko.blaise@gmail.com>
2022-04-28 12:50:55 +0100

    Add posts routes

---
+++ routes/posts.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const router = express.Router();
+
+/* GET posts index /posts */
+router.get('/',(req, res, next) => {
+  res.send('INDEX /posts');
+});
+
+/* POST posts new /posts/new */
+router.post('/new',(req, res, next) => {
+  res.send('CREATE /posts/new');
+});
+
+/* GET posts show /posts/:id */
+router.get('/:id',(req, res, next) => {
+  res.send('SHOW /posts/:id');
+});
+
+/* EDIT posts edit /posts/:id/edit */
+router.get('/:id/edit',(req, res, next) => {
+  res.send('EDIT /posts/:id/edit');
+});
+
+/* GET posts update /posts/:id */
+router.put('/:id',(req, res, next) => {
+  res.send('UPDATE /posts/:id');
+});
+
+/* GET posts delete /posts/:id */
+router.delete('/:id',(req, res, next) => {
+  res.send('DELETE /posts/:id');
+});
+
+module.exports = router;
